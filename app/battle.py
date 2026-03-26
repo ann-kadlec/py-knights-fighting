ab7d26b896f73fbf418b00fab0d64e913c98d7db
Anna Kadlecova <adorosenko440@gmail.com>
2026-03-26 12:18:41 +0100

    Fixed

--- app/battle.py
+++ app/battle.py
@@ -1,14 +1,10 @@
 from app.knight_info import Knight
 
 
-def fight(knight_1: Knight, knight_2: Knight) -> str:
+def fight(knight_1: Knight, knight_2: Knight) -> None:
     knight_1.hp -= knight_2.power - knight_1.protection
     knight_2.hp -= knight_1.power - knight_2.protection
     if knight_1.hp <= 0:
         knight_1.hp = 0
     if knight_2.hp <= 0:
         knight_2.hp = 0
-    return (
-        f"{knight_1.name} : {knight_1.hp}"
-        f"{knight_2.name} : {knight_2.hp}"
-    )
