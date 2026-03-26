f2c1e994ecf30223db4bb65728ba6d047751b957
Anna Kadlecova <adorosenko440@gmail.com>
2026-03-26 12:14:33 +0100

    New solution

--- app/battle.py
+++ app/battle.py
@@ -6,10 +6,8 @@ def fight(knight_1: Knight, knight_2: Knight) -> str:
     knight_2.hp -= knight_1.power - knight_2.protection
     if knight_1.hp <= 0:
         knight_1.hp = 0
-        print(f"{knight_2.name} won the battle!")
     if knight_2.hp <= 0:
         knight_2.hp = 0
-        print(f"{knight_1.name} won the battle!")
     return (
         f"{knight_1.name} : {knight_1.hp}"
         f"{knight_2.name} : {knight_2.hp}"
